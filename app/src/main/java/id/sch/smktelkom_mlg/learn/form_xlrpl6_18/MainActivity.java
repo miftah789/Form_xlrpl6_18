6a4e3c1dcb10f2985bf5b955c113eed879765392
miftahul jannah <miftah074@gmail.com>
2016-09-28 16:55:47 +0700

    r.id.button create doproses

--- app/src/main/java/id/sch/smktelkom_mlg/learn/form_xlrpl6_18/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/form_xlrpl6_18/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -42,6 +43,46 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void doProses() {
+        if (isValid()) {
+        }
+    }
+
+    private boolean isValid() {
+        boolean valid = true;
+        String nama = etnama.getText().toString();
+
+        if (nama.isEmpty()) {
+            etnama.setError("Nama Belum diisi");
+            valid = false;
+        } else if (nama.length() < 3) {
+            etnama.setError("Nama minimal 3 karakter");
+            valid = false;
+        } else {
+            etnama.setError(null);
+        }
+
+        String hobi = " \n";
+        int startlen = hobi.length();
+        if (baca.isChecked()) hobi += baca.getText() + "\n";
+        if (tulis.isChecked()) hobi += tulis.getText() + "\n";
+        if (dll.isChecked()) hobi += dll.getText() + "\n";
+
+        String or = null;
+        if (rgor.getCheckedRadioButtonId() != -1) {
+            RadioButton rb = (RadioButton)
+                    findViewById(rgor.getCheckedRadioButtonId());
+            or = rb.getText().toString();
+        }
+
+        if (or == null) {
+            tvhasil.setText("Belum memilih Organisasi");
+            valid = false;
+        } else {
+            tvhasil.setText(" Nama : " + nama + "\nOrganisasi yang Anda pilih :" + or + "\n\nSudah Tetdaftar Sesuai Pilihan Anda.\n");
+        }
+        return valid;
+    }
 
 
 }
